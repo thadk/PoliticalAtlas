e5708a49178e3f8beaf2244063a4dd4fb501c966
Thad Kerosky <contact@thadk.net>
2018-07-12 18:36:13 -0400

    add docs folder for ghpages

--- docs/sw.js
+++ docs/sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/PoliticalAtlas/app-35b1bbbf6b5931ef6104.js","a804bd2d9a5fccc863a6df2cbbdf67a9"],["/PoliticalAtlas/commons-88edc3cbcd2382c1076a.js","aed8e5718e30a412ee92d71da786fa98"],["/PoliticalAtlas/index.html","9b9fb5ff7547aaeff8814af7da41fe2a"],["/PoliticalAtlas/manifest.json","e92b2b9d2dda3ae16e7df792c6cf21f4"],["/PoliticalAtlas/manifest.webmanifest","f2cc56d107c98c6fb9f38fab2195bcca"],["/PoliticalAtlas/offline-plugin-app-shell-fallback/index.html","49fe0b8fcffd66c56b0067361a6d4c5f"]];
+var precacheConfig = [["/app-35b1bbbf6b5931ef6104.js","613d45bac0b64c446a52b41994058718"],["/commons-88edc3cbcd2382c1076a.js","100ad6168877f782c4ef7af31113c292"],["/index.html","a8ce79ab8bb205d24bfd400ee9625d98"],["/manifest.json","e92b2b9d2dda3ae16e7df792c6cf21f4"],["/manifest.webmanifest","f2cc56d107c98c6fb9f38fab2195bcca"],["/offline-plugin-app-shell-fallback/index.html","92d734ef4fc0e72c58a7dfec22952d77"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
