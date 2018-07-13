54c1eaba2f468cb2fb32fd8eb587fd826e9c9099
Thad Kerosky <contact@thadk.net>
2018-07-13 00:19:47 -0400

    update the sql for carto

--- carto.sql
+++ carto.sql
@@ -10,8 +10,12 @@ TO_CHAR(adu20pop_s, '9,999,999,999') as adu20pop_s_round_ca,
 
 TO_CHAR(rur20pop_s, '9,999,999,999') as rur20pop_s_round_ca,
 
+TO_CHAR(all20pop_m, '9,999,999,999') as all20pop_mean_round_ca,
+
 log(rurdense+0.00000001) as logrurdense,
 
 concat(name_2, ', ', name_1, ', ',name_0) as nicename,
 
-* FROM continents_levels2_2_pop_density
+africareg as africaregi,
+
+* FROM continents_levels2_afr_pop_density
